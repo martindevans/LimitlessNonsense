949dcf485ae40005147a38eb464d3521659f7905
Martin Evans <martindevans@gmail.com>
2026-04-06 02:28:17 +0100

    Implemented pipeline

--- LimitlessNonsense/Middleware/Pipeline.cs
+++ LimitlessNonsense/Middleware/Pipeline.cs
@@ -5,5 +5,28 @@
 /// </summary>
 public class Pipeline
 {
-    
+    private readonly IMiddleware[] _middleware;
+
+    public Pipeline(params Span<IMiddleware> middleware)
+    {
+        _middleware = middleware.ToArray();
+    }
+
+    public void Apply(MiddlewareContext context)
+    {
+        Func<MiddlewareContext, Task> next = _ => Task.CompletedTask;
+        for (var i = _middleware.Length - 1; i >= 0; i--)
+        {
+            var middleware = _middleware[i];
+            
+            // Capture the current 'next' delegate
+            var currentNext = next;
+            
+            // Create a new 'next' delegate that calls the middleware's Process method
+            next = ctx => middleware.Process(ctx, currentNext);
+        }
+
+        // Start the pipeline execution by calling the first middleware's Process method
+        next(context);
+    }
 }
