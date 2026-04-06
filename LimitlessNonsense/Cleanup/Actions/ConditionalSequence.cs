5965e3da155b17b5592d137976fe540c280f5f5c
Martin Evans <martindevans@gmail.com>
2026-04-06 23:51:26 +0100

    Early exiting ConditionalSequence

--- LimitlessNonsense/Cleanup/Actions/ConditionalSequence.cs
+++ LimitlessNonsense/Cleanup/Actions/ConditionalSequence.cs
@@ -9,9 +9,14 @@ internal record ConditionalSequence(IReadOnlyList<ContextAction> Actions)
     public override bool Execute(CleanupContext context)
     {
         var changed = false;
-        
+
         for (var i = 0; i < Actions.Count; i++)
+        {
+            if (!context.Condition.Evaluate(context.State))
+                break;
+            
             changed |= Actions[i].Execute(context);
+        }
 
         return changed;
     }
