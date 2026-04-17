ef9d961dbc41a5df1c055bdc4c21080a30e4d0c7
Martin Evans <martindevans@gmail.com>
2026-04-17 02:05:41 +0100

    Moved ISummarisationProvider to a dedicated services namespace

--- LimitlessNonsense/Cleanup/Actions/AutoSummary.cs
+++ LimitlessNonsense/Cleanup/Actions/AutoSummary.cs
@@ -151,17 +151,3 @@ public class SummarisationTask
         Cancellation.Dispose();
     }
 }
-
-/// <summary>
-/// Provides summarisation of a block of text
-/// </summary>
-public interface ISummarisationProvider
-{
-    /// <summary>
-    /// Given a block of input text, summarise/compress it.
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="cancellation"></param>
-    /// <returns></returns>
-    Task<string> Summarise(string input, CancellationToken cancellation = default);
-}
---
+++ LimitlessNonsense/Services/ISummarisationProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace LimitlessNonsense.Services;
+
+/// <summary>
+/// Provides summarisation of a block of text
+/// </summary>
+public interface ISummarisationProvider
+{
+    /// <summary>
+    /// Given a block of input text, summarise/compress it.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="cancellation"></param>
+    /// <returns></returns>
+    Task<string> Summarise(string input, CancellationToken cancellation = default);
+}
